c48b7179c0879f012c687239d7944f30d4a093dc
duhuan <duhuan>
2022-08-22 19:46:54 +0800

    初始化工程

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+
+<template>
+  <div class="app">
+    <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+// import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: {
+    // HelloWorld
+  }
+}
+</script>
+
+<style lang='less'>
+</style>
---
+++ src/page/home/login.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="login">
+    <div class="main">
+     <span class="btn-default">按钮</span>
+
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name:'Login'
+}
+</script>
+
+<style lang="scss" scoped>
+  @import '@/vr_style/login.scss'
+</style>
